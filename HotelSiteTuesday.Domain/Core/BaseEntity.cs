e37d591eaebec8f2a89733229c4f7e2458bc0189
Gabriel Ascencio <gascenciov.gav@gmail.com>
2024-02-10 18:40:28 -0400

    Arquitectura completada.

---
+++ HotelSiteTuesday.Domain/Core/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿
+namespace HotelSiteTuesday.Domain.Core
+{
+    public abstract class BaseEntity
+    {
+        public BaseEntity() 
+        {
+            this.Estado = false;
+            this.FechaCreacion = DateTime.Now;
+        }
+        public bool? Estado { get; set; }
+        public DateTime? FechaCreacion { get; set; }
+
+    }
+}
---
+++ HotelSiteTuesday.IntegrationTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace HotelSiteTuesday.IntegrationTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
