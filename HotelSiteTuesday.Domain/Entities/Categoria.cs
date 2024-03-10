bbe1a5f4558259bf395a8e4a2e19a34ffbdb1d14
franchequita <francheska.guillermo2109@gmail.com>
2024-03-10 13:50:26 -0700

    cambios categoria

---
+++ HotelSiteTuesday.Domain/Entities/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSiteTuesday.Domain.Entities
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string Descripcion{ get; set; }
+        public Boolean Estado { get; set; }
+        public DateTime FechaCreacion { get; set; }
+    }
+}
